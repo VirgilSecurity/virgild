39d5cdb6d1ddb8ee8f9588f8e5810bdfb8e26bb5
tochka <vinogradovve@gmail.com>
2016-11-03 17:52:59 +0500

    added signer

---
+++ signer/signer.go
@@ -0,0 +1,23 @@
+package signer
+
+import (
+	"github.com/virgilsecurity/virgil-apps-cards-cacher/models"
+	"gopkg.in/virgilsecurity/virgil-sdk-go.v4"
+	"gopkg.in/virgilsecurity/virgil-sdk-go.v4/virgilcrypto"
+)
+
+type ServiceSigner struct {
+	ID         string
+	PrivateKey virgilcrypto.PrivateKey
+}
+
+func (s *ServiceSigner) Sign(r *models.CardResponse) error {
+	crypto := virgil.Crypto()
+	sign, err := crypto.Sign(r.Snapshot, s.PrivateKey)
+	if r.Meta.Signatures == nil {
+		r.Meta.Signatures = make(map[string][]byte, 0)
+	}
+	r.Meta.Signatures[s.ID] = sign
+	return err
+	// return nil
+}
