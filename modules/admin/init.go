f3835e0f8823df139db17ba7ba6b8c488907d001
Vladimir Vinogradov <vinogradovve@gmail.com>
2017-02-01 09:00:37 +0500

    Added admin api function

--- modules/admin/init.go
+++ modules/admin/init.go
@@ -1,6 +1,8 @@
 package admin
 
 import (
+	"encoding/json"
+	"fmt"
 	"io/ioutil"
 
 	"github.com/VirgilSecurity/virgild/config"
@@ -8,12 +10,18 @@ import (
 )
 
 type AdminHandlers struct {
-	Index fasthttp.RequestHandler
+	Index        fasthttp.RequestHandler
+	GetConfig    fasthttp.RequestHandler
+	UpdateConfig fasthttp.RequestHandler
+	CardInfo     fasthttp.RequestHandler
 }
 
 func Init(conf *config.App) *AdminHandlers {
 	return &AdminHandlers{
-		Index: mainPage("./templates/index.html"),
+		Index:        mainPage("./templates/index.html"),
+		GetConfig:    getConf(conf.Common.ConfigUpdate),
+		CardInfo:     getVirgilDCardInfo(conf.Site.VirgilD),
+		UpdateConfig: updateConf(conf.Common.ConfigUpdate),
 	}
 }
 
@@ -26,5 +34,45 @@ func mainPage(path string) fasthttp.RequestHandler {
 		}
 		ctx.SetContentType("text/html; charset=UTF-8")
 		ctx.Write(f)
+	}
+}
+
+func getConf(updater *config.Updater) fasthttp.RequestHandler {
+	return func(ctx *fasthttp.RequestCtx) {
+		b, err := json.Marshal(updater.Config())
+		if err != nil {
+			ctx.Error("", fasthttp.StatusInternalServerError)
+			return
+		}
+		ctx.SetContentType("application/json")
+		ctx.Write(b)
+	}
+}
+
+func updateConf(updater *config.Updater) fasthttp.RequestHandler {
+	return func(ctx *fasthttp.RequestCtx) {
+		ctx.SetContentType("application/json")
+		conf := new(config.Config)
+		err := json.Unmarshal(ctx.PostBody(), conf)
+		if err != nil {
+			ctx.Error("{'message':'JSON invalid'}", fasthttp.StatusBadRequest)
+			return
+		}
+		err = updater.Update(*conf)
+		if err != nil {
+			ctx.Error(fmt.Sprintf("{'message':'%v'}", err), fasthttp.StatusBadRequest)
+		}
+	}
+}
+
+func getVirgilDCardInfo(info config.VirgilDCard) fasthttp.RequestHandler {
+	return func(ctx *fasthttp.RequestCtx) {
+		b, err := json.Marshal(info)
+		if err != nil {
+			ctx.Error("", fasthttp.StatusInternalServerError)
+			return
+		}
+		ctx.SetContentType("application/json")
+		ctx.Write(b)
 	}
 }
